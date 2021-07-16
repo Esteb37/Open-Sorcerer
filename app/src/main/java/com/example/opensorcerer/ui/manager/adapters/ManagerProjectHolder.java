a600345c0213092da25daf95e42e5f6e07eb35de
Esteb37 <esteban37padilla@gmail.com>
2021-07-16 12:46:02 -0500

    JavaDoc, comments and cleanup for manager recyclerview adapters

--- app/src/main/java/com/example/opensorcerer/ui/manager/adapters/ManagerProjectHolder.java
+++ app/src/main/java/com/example/opensorcerer/ui/manager/adapters/ManagerProjectHolder.java
@@ -1,25 +1,25 @@
 package com.example.opensorcerer.ui.manager.adapters;
 
-import android.app.Activity;
 import android.content.Context;
 import android.view.View;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.example.opensorcerer.databinding.ItemManagerProjectBinding;
 import com.example.opensorcerer.models.Project;
 import com.parse.ParseFile;
 
+/**
+ * ViewHolder class for Projects created by the manager
+ */
 @SuppressWarnings({"unused", "FieldCanBeLocal"})
 public class ManagerProjectHolder extends RecyclerView.ViewHolder{
 
-
-    //View binder
+    /**Binder object for ViewBinding*/
     private final ItemManagerProjectBinding app;
 
-    //Current context
+    /**The Holder's context*/
     private final Context mContext;
 
     public ManagerProjectHolder(View view, Context context, ItemManagerProjectBinding binder) {
@@ -28,13 +28,20 @@ public class ManagerProjectHolder extends RecyclerView.ViewHolder{
         mContext = context;
     }
 
+    /**
+     * Populates the view's items with the project's information
+     * @param project The project to display
+     */
     public void bind(Project project) {
 
+        //Set text details
         app.tvTitle.setText(project.getTitle());
         app.tvDescription.setText(project.getDescription());
 
+        //Dynamically set the title's max lines to avoid word breaking
         app.tvTitle.setMaxLines(project.getTitle().split(" ").length);
 
+        //Load the project's image
         ParseFile image = project.getLogoImage();
         if(image != null){
             Glide.with(mContext)
--- app/src/main/java/com/example/opensorcerer/ui/manager/adapters/ManagerProjectsAdapter.java
+++ app/src/main/java/com/example/opensorcerer/ui/manager/adapters/ManagerProjectsAdapter.java
@@ -10,48 +10,83 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.opensorcerer.databinding.ItemManagerProjectBinding;
 import com.example.opensorcerer.models.Project;
+import com.example.opensorcerer.ui.developer.adapters.ProjectHolder;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
+/**
+ * RecyclerView Adapter class for Projects in the Developer's timeline
+ */
 @SuppressWarnings({"unused", "FieldCanBeLocal"})
-public class ManagerProjectsAdapter extends RecyclerView.Adapter<ManagerProjectHolder>{
+public class ManagerProjectsAdapter extends RecyclerView.Adapter<ManagerProjectHolder> {
 
-    List<Project> mProjects;
-    Context mContext;
+    /**
+     * Binder object for ViewBinding
+     */
+    private ItemManagerProjectBinding app;
 
-    ItemManagerProjectBinding app;
+    /**
+     * The adapter's current context
+     */
+    private final Context mContext;
+
+    /**
+     * The list of projects to display
+     */
+    private final List<Project> mProjects;
+
+    /**
+     * The ViewHolder for the project items
+     */
+    private ProjectHolder mHolder;
 
     public ManagerProjectsAdapter(List<Project> projects, Context context) {
         mProjects = projects;
         mContext = context;
     }
 
+    /**
+     * Inflates the ViewHolder and prepares it for the items
+     *
+     * @return the inflated ProjectHolder
+     */
     @NonNull
     @NotNull
     @Override
     public ManagerProjectHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
         app = ItemManagerProjectBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
         View view = app.getRoot();
-        return new ManagerProjectHolder(view,mContext, app);
+        return new ManagerProjectHolder(view, mContext, app);
     }
 
+    /**
+     * Binds the project to a ViewHolder to display it
+     */
     @Override
     public void onBindViewHolder(@NonNull @NotNull ManagerProjectHolder holder, int position) {
         holder.bind(mProjects.get(position));
     }
 
+    /**Project list item count getter*/
     @Override
     public int getItemCount() {
         return mProjects.size();
     }
 
+    /**
+     * Cleans the project list and notifies the adapter
+     */
     public void clear() {
         mProjects.clear();
         notifyDataSetChanged();
     }
 
+    /**
+     * Adds a list of projects to the adapter
+     * @param list A list of projects
+     */
     public void addAll(List<Project> list) {
         mProjects.addAll(list);
         notifyDataSetChanged();
