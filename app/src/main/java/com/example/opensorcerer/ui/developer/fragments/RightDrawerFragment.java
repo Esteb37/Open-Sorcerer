5522cbd6fc70700902f926f5b05a7b19c1b3ddf5
Esteb37 <esteban37padilla@gmail.com>
2021-07-21 13:55:54 -0500

    Drawer for Profile view menu

---
+++ app/src/main/java/com/example/opensorcerer/ui/developer/fragments/RightDrawerFragment.java
@@ -0,0 +1,18 @@
+package com.example.opensorcerer.ui.developer.fragments;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.opensorcerer.R;
+
+public class RightDrawerFragment extends androidx.fragment.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_right_drawer,container,false);
+        return view;
+    }
+}
