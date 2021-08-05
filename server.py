61de6f128dbb9b0c71953554f1cc0556122caa2e
Esteb37 <esteban37padilla@gmail.com>
2021-08-04 21:54:48 -0500

    Added record to pick up from last scraped project

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
-import json
-import requests
-import urllib
 from flask import Flask
-from flask_restful import Api, Resource
-from requests.structures import CaseInsensitiveDict
+from flask_restful import Api,Resource
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import os
+import requests
+import json
+from requests.structures import CaseInsensitiveDict
+import json,httplib,urllib
+import datetime
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -22,6 +27,8 @@ githubHeaders["Authorization"] = "Basic ZXN0ZWIzNzpnaHBfZmRYWnRodTV3c085emt6c2ND
 githubHeaders["Accept"] = "application/vnd.github.mercy-preview+json"
 githubAPI = "https://api.github.com/repos/"
 
+scraperRecord = {}
+
 """
     Class for handling the project scraper
 """
@@ -32,155 +39,185 @@ class ScrapeProjects(Resource):
     """
     def post(self):
 
+        file = open("scraperRecord.txt")
+        scraperRecord = eval(file.read())
+        file.close()
+
         # Launch the chrome web driver
         path = r'/Users/estebanpadilla/Desktop/Open Sorcerer REST/chromedriver'
-        driver = webdriver.Chrome(executable_path=path)
+        driver = webdriver.Chrome(executable_path = path)
 
-        categories = ["featured", "cloud", "analytics-visualization", "databases",
-                      "developer-tools", "education", "enterprise", "games", "graphics-video-audio",
-                      "i18n", "iot", "mobile", "machine-learning", "geo", "networking",
-                      "programming", "samples", "security", "testing", "utilities", "web"]
+        categories = ["featured", "cloud", "analytics-visualization", "databases", "developer-tools", "education", "enterprise", "games", "graphics-video-audio",
+                      "i18n", "iot", "mobile", "machine-learning", "geo", "networking", "programming", "samples", "security", "testing", "utilities", "web"]
 
         # For each category
-        for category in categories:
-            print("----" + category + "----")
+        for category in categories[categories.index(scraperRecord["last_category"]) : ]:
+            print("----"+category+"----")
+            scraperRecord["last_category"] = category
 
             # Load the catalogue
-            driver.get('https://opensource.google/projects/list/' + category)
+            driver.get('https://opensource.google/projects/list/'+category)
 
             # Get a list of project card items
             projects = []
-            while (len(projects) == 0):
+            while(len(projects) == 0):
                 projects = driver.find_elements_by_class_name("project-card")
 
             # Get the links to each project
             links = [project.get_attribute('href') for project in projects]
 
+            lastIndex = scraperRecord["last_index"]
+
+            links = links[lastIndex : ]
+
+
+
             # For each project link
-            for link in links:
+            for index, link in enumerate(links):
+
+                scraperRecord["last_index"] = index + lastIndex
+
+                file = open("scraperRecord.txt", "w")
+                file.write(str(scraperRecord))
+                file.close()
 
                 # Load the project's page
                 driver.get(link)
 
                 repo = ""
                 image = ""
-                while (repo == ""):
+                while(repo == ""):
 
                     # Find all table cells
                     tds = driver.find_elements_by_tag_name("td")
 
-                    if (len(tds) > 0):
+                    if(len(tds) > 0):
 
                         # Get the link from the first table cell, which contains the repository link
                         repo = tds[0].find_element_by_tag_name("a").get_attribute('href')
 
                         # Try to get the image from the logo, if there is none, assign Google Open Source's logo
                         try:
-                            image = driver.find_element_by_class_name(
-                                "logo").find_element_by_tag_name("img").get_attribute("src")
+                            image = driver.find_element_by_class_name("logo").find_element_by_tag_name("img").get_attribute("src")
                         except:
                             image = "https://www.mathieuchartier.com/wp-content/uploads/blog_image_1.jpg"
 
                 # If the repository link is from github
-                if (githubURL in repo):
+                if(githubURL in repo):
 
                     # Get the repository name
                     reponame = repo.split(githubURL)[1]
 
-                    if (projectIsNotCreated(reponame)):
+                    if(projectIsNotCreated(reponame)):
 
                         try:
 
                             # Create a project from the repository data
-                            data = requests.get(githubAPI + reponame, headers=githubHeaders).json()
+                            data = requests.get(githubAPI+reponame, headers = githubHeaders).json()
                             createProject(data, repo, image)
 
                         except ValueError:
-                            print("No data", repo)
+                            print("No data",repo)
 
                     else:
                         print("Project already exists.")
 
-        return {"data": "Completed"}
+            scraperRecord["last_index"] = 0
 
-    """
-        Determines if the project is already in the database
-    """
-    def projectIsNotCreated(reponame):
+        scraperRecord["last_time"] = datetime.datetime.now()
+        scraperRecord["last_category"] = categories[0]
 
-        try:
+        file = open("scraperRecord.txt", "w")
+        file.write(str(scraperRecord))
+        file.close()
 
-            # Make a query for the project by its repository name
-            params = urllib.urlencode({"where": json.dumps({
-                "githubName": reponame
-            })})
+        return {"data":"Completed"}
 
-            url = serverURL + "classes/Project/?" + params
+"""
+    Determines if the project is already in the database
+"""
+def projectIsNotCreated(reponame):
 
-            resp = requests.get(url, headers=parseHeaders)
+    try:
 
-            # Return if the query threw any results
-            return (resp.json()["results"] == [])
+        # Make a query for the project by its repository name
+        params = urllib.urlencode({"where":json.dumps({
+            "githubName":reponame
+        })})
 
-        except KeyError:
-            return True
+        url = serverURL + "classes/Project/?" + params
 
-    """
-        Creates a project in the Parse Database from the repository information
-    """
-    def createProject(data, repo, image):
+        resp = requests.get(url, headers=parseHeaders)
 
-        reponame = repo.split(githubURL)[1]
+        # Return if the query threw any results
+        return (resp.json()["results"] == [])
 
-        # Get the project's information
-        try:
-            title = data["name"].replace("_", " ").capitalize()
+    except KeyError:
+        return True
 
-        except KeyError:
-            print("No name", repo)
-            return
+"""
+    Creates a project in the Parse Database from the repository information
+"""
+def createProject(data, repo, image):
 
-        try:
-            description = data["description"]
-        except:
-            description = ""
+    reponame = repo.split(githubURL)[1]
 
-        try:
-            homepage = data["homepage"]
-            if (len(homepage) == 0):
-                homepage = repo
-        except:
+    # Get the project's information
+    try:
+        title = data["name"].replace("_"," ").capitalize()
+
+    except KeyError:
+        print("No name", repo)
+        return
+
+    try:
+        description = data["description"]
+    except:
+        description = ""
+
+    try:
+        homepage = data["homepage"]
+        if(len(homepage) == 0):
             homepage = repo
+    except:
+        homepage = repo
 
-        try:
-            resp = requests.get(githubAPI + reponame + "/topics", headers=githubHeaders)
-            tags = resp.json()["names"]
-        except:
-            tags = []
 
-        try:
-            resp = requests.get(githubAPI + reponame + "/languages", headers=githubHeaders)
-            languages = resp.json().keys()
-        except:
-            languages = []
+    try:
+        resp = requests.get(githubAPI + reponame + "/topics", headers = githubHeaders)
+        tags = resp.json()["names"]
+    except:
+        tags = []
 
-        # Set the project's information
-        project = CaseInsensitiveDict()
-        project["title"] = title
-        project["description"] = description
-        project["website"] = homepage
-        project["tags"] = tags
-        project["languages"] = languages
-        project["repository"] = repo
-        project["logoUrl"] = image
-        project["githubName"] = reponame
+    try:
+        resp = requests.get(githubAPI + reponame + "/languages", headers = githubHeaders)
+        languages = resp.json().keys()
+    except:
+        languages = []
 
-        # Save the project in the database
-        url = serverURL + "classes/Project/"
-        resp = requests.post(url, headers=parseHeaders, data=project)
 
+    # Set the project's information
+    project = CaseInsensitiveDict()
+    project["title"] = title
+    project["description"] = description
+    project["website"] = homepage
+    project["tags"] = tags
+    project["languages"] = languages
+    project["repository"] = repo
+    project["logoUrl"] = image
+    project["githubName"] = reponame
+
+    # Save the project in the database
+    url = serverURL + "classes/Project/"
+    resp = requests.post(url, headers = parseHeaders, data = project)
+
+    print("Created project " + title)
 
 api.add_resource(ScrapeProjects, "/scrape_projects")
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug = False, host='0.0.0.0')
+
+
+
+
